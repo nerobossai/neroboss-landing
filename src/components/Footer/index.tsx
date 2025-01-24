20b7efda18c6f8c06719e9990f42f73510e57db3
Abhishek Vishwakarma <abhishek.vishwakarma9@gmail.com>
2025-01-25 01:18:28 +0530

    feat: finished footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,10 @@
-import { Center, HStack, Link, Text } from "@chakra-ui/react";
+import { HStack, Link, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 import styled from "styled-components";
 
 import { Paths } from "@/constants/paths";
+
+import { ServiceStatus } from "../Svgs/ServiceStatus";
 
 const Container = styled.footer`
   width: 100%;
@@ -14,19 +16,68 @@ const Container = styled.footer`
 function Footer() {
   return (
     <Container>
-      <Center width="100%">
-        <HStack spacing={8} color="primary.600">
-          <Link href={Paths.twitter} target="_blank">
-            <Text>Twitter</Text>
-          </Link>
-          <Link href={Paths.telegram} target="_blank">
-            <Text>Telegram</Text>
-          </Link>
-          <Link href={Paths.github} target="_blank">
-            <Text>GitHub</Text>
-          </Link>
+      <HStack
+        width="100%"
+        justifyContent="space-between"
+        alignItems="center"
+        fontSize="13px"
+        flexDirection={["column", "row", "row"]}
+      >
+        <Stack
+          justifyContent="center"
+          alignItems="center"
+          paddingLeft={["0rem", "2rem", "2rem"]}
+          paddingBottom={["1rem", "2rem", "2rem"]}
+        >
+          <ServiceStatus />
+          <Text opacity={0.5}>
+            Copyright © All rights by Keygen Labs, Inc.
+          </Text>
+        </Stack>
+        <HStack
+          paddingRight={["0rem", "2rem", "2rem"]}
+          flexDirection={["column", "row", "row"]}
+          spacing={["2rem", "5rem", "5rem"]}
+          alignItems="flex-start"
+        >
+          <Stack>
+            <Text opacity={0.5}>COMMUNITY</Text>
+            <Link href={Paths.twitter} target="_blank">
+              <Text>X</Text>
+            </Link>
+            <Link href={Paths.telegram} target="_blank">
+              <Text>Telegram</Text>
+            </Link>
+            <Link href={Paths.github} target="_blank">
+              <Text>GitHub</Text>
+            </Link>
+          </Stack>
+          <Stack>
+            <Text opacity={0.5}>LINKS</Text>
+            <Link href={Paths.raydium} target="_blank">
+              <Text>Buy NEROBOSS</Text>
+            </Link>
+            <Link href={Paths.app} target="_blank">
+              <Text>Open Nerocity</Text>
+            </Link>
+            <Link href={Paths.docs} target="_blank">
+              <Text>Read Docs</Text>
+            </Link>
+            <Link href={Paths.github} target="_blank">
+              <Text>Star on GitHub</Text>
+            </Link>
+          </Stack>
+          {/* <Stack>
+            <Text opacity={0.5}>LEGAL</Text>
+            <Link href={Paths.twitter} target="_blank">
+              <Text>Terms of Service</Text>
+            </Link>
+            <Link href={Paths.telegram} target="_blank">
+              <Text>Privacy Policy</Text>
+            </Link>
+          </Stack> */}
         </HStack>
-      </Center>
+      </HStack>
     </Container>
   );
 }
--- src/components/Svgs/ServiceStatus.tsx
+++ src/components/Svgs/ServiceStatus.tsx
@@ -1,74 +1,98 @@
 export const ServiceStatus = () => (
   <svg
-    width="286"
-    height="77"
-    viewBox="0 0 286 77"
+    width="337"
+    height="181"
+    viewBox="0 0 400 181"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
       opacity="0.1"
-      d="M247.5 0.5H38.5C17.5132 0.5 0.5 17.5132 0.5 38.5C0.5 59.4868 17.5132 76.5 38.5 76.5H247.5C268.487 76.5 285.5 59.4868 285.5 38.5C285.5 17.5132 268.487 0.5 247.5 0.5Z"
+      d="M298.5 52.5H89.5C68.5132 52.5 51.5 69.5132 51.5 90.5C51.5 111.487 68.5132 128.5 89.5 128.5H298.5C319.487 128.5 336.5 111.487 336.5 90.5C336.5 69.5132 319.487 52.5 298.5 52.5Z"
       fill="url(#paint0_linear_0_1)"
-      fill-opacity="0.05"
+      fillOpacity="0.05"
       stroke="url(#paint1_linear_0_1)"
     />
     <path
       opacity="0.2"
-      d="M247.5 12.5H38.5C24.1406 12.5 12.5 24.1406 12.5 38.5C12.5 52.8594 24.1406 64.5 38.5 64.5H247.5C261.859 64.5 273.5 52.8594 273.5 38.5C273.5 24.1406 261.859 12.5 247.5 12.5Z"
+      d="M298.5 64.5H89.5C75.1406 64.5 63.5 76.1406 63.5 90.5C63.5 104.859 75.1406 116.5 89.5 116.5H298.5C312.859 116.5 324.5 104.859 324.5 90.5C324.5 76.1406 312.859 64.5 298.5 64.5Z"
       fill="url(#paint2_linear_0_1)"
-      fill-opacity="0.05"
+      fillOpacity="0.05"
       stroke="url(#paint3_linear_0_1)"
     />
     <path
-      d="M248.5 24H39.5C31.4919 24 25 30.4919 25 38.5C25 46.5081 31.4919 53 39.5 53H248.5C256.508 53 263 46.5081 263 38.5C263 30.4919 256.508 24 248.5 24Z"
+      d="M299.5 76H90.5C82.4919 76 76 82.4919 76 90.5C76 98.5081 82.4919 105 90.5 105H299.5C307.508 105 314 98.5081 314 90.5C314 82.4919 307.508 76 299.5 76Z"
       fill="url(#paint4_radial_0_1)"
-      fill-opacity="0.2"
+      fillOpacity="0.2"
     />
+    <g opacity="0.8" filter="url(#filter0_f_0_1)">
+      <path
+        d="M76 90C76 82.268 82.268 76 90 76C97.732 76 104 82.268 104 90V91C104 98.732 97.732 105 90 105C82.268 105 76 98.732 76 91V90Z"
+        fill="#24E98A"
+      />
+    </g>
     <defs>
+      <filter
+        id="filter0_f_0_1"
+        x="0"
+        y="0"
+        width="180"
+        height="181"
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        />
+        <feGaussianBlur stdDeviation="38" result="effect1_foregroundBlur_0_1" />
+      </filter>
       <linearGradient
         id="paint0_linear_0_1"
-        x1="59.492"
-        y1="20.3396"
-        x2="151.242"
-        y2="55.484"
+        x1="110.492"
+        y1="72.3396"
+        x2="202.242"
+        y2="107.484"
         gradientUnits="userSpaceOnUse"
       >
         <stop stopColor="#0EA65E" />
-        <stop offset="1" stopColor="#0EA65E" stop-opacity="0" />
+        <stop offset="1" stopColor="#0EA65E" stopOpacity="0" />
       </linearGradient>
       <linearGradient
         id="paint1_linear_0_1"
-        x1="56.5"
-        y1="7.5"
-        x2="87.5"
-        y2="59.5"
+        x1="107.5"
+        y1="59.5"
+        x2="138.5"
+        y2="111.5"
         gradientUnits="userSpaceOnUse"
       >
         <stop stopColor="#232A27" />
-        <stop offset="1" stopColor="#232A27" stop-opacity="0" />
+        <stop offset="1" stopColor="#232A27" stopOpacity="0" />
       </linearGradient>
       <linearGradient
         id="paint2_linear_0_1"
-        x1="66.5"
-        y1="26"
-        x2="143"
-        y2="65"
+        x1="117.5"
+        y1="78"
+        x2="194"
+        y2="117"
         gradientUnits="userSpaceOnUse"
       >
         <stop stopColor="#0EA65E" />
-        <stop offset="1" stopColor="#0EA65E" stop-opacity="0" />
+        <stop offset="1" stopColor="#0EA65E" stopOpacity="0" />
       </linearGradient>
       <linearGradient
         id="paint3_linear_0_1"
-        x1="48.5"
-        y1="16.5"
-        x2="69"
-        y2="49"
+        x1="99.5"
+        y1="68.5"
+        x2="120"
+        y2="101"
         gradientUnits="userSpaceOnUse"
       >
         <stop stopColor="#232A27" />
-        <stop offset="1" stopColor="#232A27" stop-opacity="0" />
+        <stop offset="1" stopColor="#232A27" stopOpacity="0" />
       </linearGradient>
       <radialGradient
         id="paint4_radial_0_1"
@@ -76,10 +100,10 @@ export const ServiceStatus = () => (
         cy="0"
         r="1"
         gradientUnits="userSpaceOnUse"
-        gradientTransform="translate(41.5 41.5758) rotate(2.73015) scale(64.5733 53.2506)"
+        gradientTransform="translate(92.5 93.5758) rotate(2.73015) scale(64.5733 53.2506)"
       >
         <stop stopColor="#0BE27B" />
-        <stop offset="1" stopColor="#161616" />
+        <stop offset="1" stopColor="#000000" />
       </radialGradient>
     </defs>
   </svg>
